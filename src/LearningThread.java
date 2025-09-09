a7168972e786d9be6b253f99ba9818f01726de56
rajeevansharan <rajeevansharan@gmail.com>
2025-09-10 00:50:21 +0530

    learned about thread and runnable

---
+++ src/LearningThread.java
@@ -0,0 +1,28 @@
+class MyThread extends Thread {
+    public void run() {
+        int a;
+        for (a = 1; a <= 10; a++) {
+            System.out.println("Hii");
+        }
+
+    }
+}
+
+class MyRunnable implements Runnable {
+    public void run() {
+        System.out.println("Sharan");
+    }
+}
+
+
+public class LearningThread {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        Runnable run = new MyRunnable();
+        Thread t2 = new Thread(run);
+
+        t1.start();
+        t2.start();
+
+    }
+}
